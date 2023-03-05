28fadca588296421c2360d432f072d38e08b6eb3
Skyler Grey <skyler3665@gmail.com>
2023-03-05 07:47:00 +0000

    Update the workflow to use the right node version

--- flake.nix
+++ flake.nix
@@ -8,7 +8,7 @@
       pkgs = nixpkgs.legacyPackages.${system};
     in {
       devShells.default = pkgs.mkShell {
-        packages = [ pkgs.yarn pkgs.nodejs-19_x pkgs.typescript ];
+        packages = [ pkgs.yarn pkgs.nodejs-19_x pkgs.typescript pkgs.act ];
       };
     });
 }
