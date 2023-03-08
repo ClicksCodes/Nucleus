023d8253ec8b1d50781cb74683d6d718a0dec40c
Skyler Grey <skyler3665@gmail.com>
2023-03-08 14:52:41 +0000

    Fix the recursve generateFileName async function

--- src/utils/temp/generateFileName.ts
+++ src/utils/temp/generateFileName.ts
@@ -10,7 +10,7 @@ export default async function generateFileName(ending: string): Promise<string> 
     let fileName = crypto.randomBytes(35).toString("hex");
     fileName = fileName.replace(/([a-zA-Z0-9]{8})/g, "$1-");
     if (fs.existsSync(`./${fileName}`)) {
-        fileName = generateFileName(ending);
+        fileName = await generateFileName(ending);
     }
     await client.database.eventScheduler.schedule("deleteFile", (Date.now() + 60 * 1000).toString(), {
         fileName: `${fileName}.${ending}`
