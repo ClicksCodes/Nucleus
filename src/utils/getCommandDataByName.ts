6a0bab53196cbd90f22546e32b3b7c2d40c53f86
Skyler Grey <skyler3665@gmail.com>
2023-03-15 00:10:26 +0000

    Reformat code

--- src/utils/getCommandDataByName.ts
+++ src/utils/getCommandDataByName.ts
@@ -2,11 +2,11 @@ import type Discord from "discord.js";
 import client from "./client.js";
 
 /**
-* @param name The name of the command, not including a leading slash. This can be space or slash separated e.g. "mod/about" or "mod about"
-* @returns A string which when put into Discord will mention the command if the command exists or a codeblock with the command name if it does not
-*
-* @throws Will throw an error if as empty string is passed
-**/
+ * @param name The name of the command, not including a leading slash. This can be space or slash separated e.g. "mod/about" or "mod about"
+ * @returns A string which when put into Discord will mention the command if the command exists or a codeblock with the command name if it does not
+ *
+ * @throws Will throw an error if as empty string is passed
+ **/
 export const getCommandMentionByName = (name: string): string => {
     const split = name.replaceAll("/", " ").split(" ");
     const commandName: string | undefined = split[0];
@@ -23,12 +23,12 @@ export const getCommandMentionByName = (name: string): string => {
 };
 
 /**
-* @param name The name of the command, not including a leading slash. This can be space or slash separated e.g. "mod/about" or "mod about"
-* @returns An object containing the command name, the command description and a string which when put into Discord will mention the command
-*
-* @throws Will throw an error if the command doesn't exist
-* @throws Will throw an error if as empty string is passed
-**/
+ * @param name The name of the command, not including a leading slash. This can be space or slash separated e.g. "mod/about" or "mod about"
+ * @returns An object containing the command name, the command description and a string which when put into Discord will mention the command
+ *
+ * @throws Will throw an error if the command doesn't exist
+ * @throws Will throw an error if as empty string is passed
+ **/
 export const getCommandByName = (name: string): { name: string; description: string; mention: string } => {
     const split = name.replaceAll(" ", "/");
     const command = client.commands["commands/" + split];
