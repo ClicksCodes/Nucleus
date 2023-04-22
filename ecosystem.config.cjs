92a2a1a272e7f54aa12fc562da1c2ef9d0413843
TheCodedProf <samuel.shuert@gmail.com>
2023-04-22 19:58:18 -0400

    prettier

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,7 +1,9 @@
 module.exports = {
-    apps: [{
-        name: "Nucleus",
-        script: "/run/current-system/sw/bin/nix",
-        args: "develop --command yarn start --update-commands",
-    }],
-}
+    apps: [
+        {
+            name: "Nucleus",
+            script: "/run/current-system/sw/bin/nix",
+            args: "develop --command yarn start --update-commands"
+        }
+    ]
+};
