a0e072a3c10d1bf2bff6e904d332d74489fdcae5
TheCodedProf <samuel.shuert@gmail.com>
2023-04-22 19:57:56 -0400

    added pm2 ecosystem file

---
+++ ecosystem.config.cjs
@@ -0,0 +1,7 @@
+module.exports = {
+    apps: [{
+        name: "Nucleus",
+        script: "/run/current-system/sw/bin/nix",
+        args: "develop --command yarn start --update-commands",
+    }],
+}
--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   description = "A basic flake with a shell";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
@@ -9,6 +9,9 @@
     in {
       devShells.default = pkgs.mkShell {
         packages = [ pkgs.nodejs-19_x pkgs.nodePackages_latest.yarn ];
+        shellHook = ''
+          unset name
+        '';
       };
     });
 }
